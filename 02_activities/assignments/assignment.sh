a8740561f8299d34dd67c7f3ade6db04b23f0353
Michelle Lin <239458609+michelledsicohort8@users.noreply.github.com>
2025-11-08 09:34:26 -0500

    updated

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -55,8 +55,9 @@ rm data/raw/*ipaddr*
 rm data/processed/user_logs/*ipaddr*
 
 # # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
-cd data 
+cd data
 touch inventory.txt
+cd ../
 ls data/processed/server_logs >> data/inventory.txt
 ls data/processed/event_logs >> data/inventory.txt
 ls data/processed/user_logs >> data/inventory.txt
